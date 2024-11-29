100fea895f4f3644e23bfdfa70ce54c6cf945da0
Nojan0727 <9025261@student.zadkine.nl>
2024-11-29 19:03:50 +0100

    switch hobby html to php

--- Hobby.php
+++ Hobby.php
@@ -1,24 +1,4 @@
-<!doctype html>
-<html lang="en">
-     <head>
-       <meta charset="UTF-8">
-       <meta name="viewport" content="Hobby">
-       <title>Hobby</title>
-       <link rel="stylesheet" href="main.css">
-         <script defer src="script.js"></script>
-     </head>
-<body class="body-background">
-     <nav class="navbar">
-        <a class="nav-link" href="Index.php"><img src="image/Logo.png" alt="Logo" class="nav-logo"></a>
-        <a class="nav-link" href="About_me.php">About me</a>
-        <a class="nav-link" href="Professional_Experience.php">Professional experiences</a>
-        <a class="nav-link" href="Skills.php">Skills</a>
-        <a class="nav-link active" href="Hobby.php">Hobby</a>
-        <a class="nav-link" href="Certifications.php">Certifications</a>
-        <a class="nav-link" href="Interview.php">Interview</a>
-        <a class="nav-link" href="Thoughts.php">Thoughts</a>
-        <a class="nav-link" href="Contact.php">Contact</a>
-     </nav>
+<?php require "partials/header.php" ?>
 <main class="hobby-layout">
   <section class="hobby-text">
     <h1 class="main-hobby">Karting Enthusiast</h1>
