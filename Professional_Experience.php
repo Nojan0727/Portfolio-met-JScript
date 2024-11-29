3bb7cd2f31a8c99dbcce7ee63e407512fb3b56b2
Nojan0727 <9025261@student.zadkine.nl>
2024-11-29 19:04:38 +0100

    switch experience html to php

--- Professional_Experience.php
+++ Professional_Experience.php
@@ -1,24 +1,4 @@
-<!doctype html>
-<html lang="en">
-    <head>
-         <meta charset="UTF-8">
-         <meta name="viewport" content="Professional Experience page of my portfolio">
-         <title>Professional Experience</title>
-         <link rel="stylesheet" href="main.css">
-        <script defer src="script.js"></script>
-    </head>
-<body class="body-background">
-    <nav class="navbar">
-          <a class="nav-link" href="Index.php"><img src="image/Logo.png" alt="Logo" class="nav-logo"></a>
-          <a class="nav-link" href="About_me.php">About me</a>
-          <a class="nav-link active" href="Professional_Experience.php">Professional experiences</a>
-          <a class="nav-link" href="Skills.php">Skills</a>
-          <a class="nav-link" href="Hobby.php">Hobby</a>
-          <a class="nav-link" href="Certifications.php">Certifications</a>
-          <a class="nav-link" href="Interview.php">Interview</a>
-          <a class="nav-link" href="Thoughts.php">Thoughts</a>
-          <a class="nav-link" href="Contact.php">Contact</a>
-    </nav>
+<?php require "partials/header.php" ?>
     <div class="container-center">
         <h1 class="main-heading"> Professional Experience </h1>
         <h2 class="second-heading"> Accountant (2016-2018)</h2>
