6c40b32a397d15a77c5c9908bd37824f1413ac87
Nojan0727 <9025261@student.zadkine.nl>
2024-11-29 19:04:13 +0100

    switch interview html to php

--- Interview.php
+++ Interview.php
@@ -1,24 +1,4 @@
-<!doctype html>
-<html lang="en">
-      <head>
-         <meta charset="UTF-8">
-         <meta name="viewport" content="Interview">
-         <title>Interview</title>
-         <link rel="stylesheet" href="main.css">
-          <script defer src="script.js"></script>
-      </head>
-<body class="body-background">
-      <nav class="navbar">
-          <a class="nav-link" href="Index.php"><img src="image/Logo.png" alt="Logo" class="nav-logo"></a>
-          <a class="nav-link" href="About_me.php">About me</a>
-          <a class="nav-link" href="Professional_Experience.php">Professional experiences</a>
-          <a class="nav-link" href="Skills.php">Skills</a>
-          <a class="nav-link" href="Hobby.php">Hobby</a>
-          <a class="nav-link" href="Certifications.php">Certifications</a>
-          <a class="nav-link active" href="Interview.php">Interview</a>
-          <a class="nav-link" href="Thoughts.php">Thoughts</a>
-          <a class="nav-link" href="Contact.php">Contact</a>
-      </nav>
+<?php require "partials/header.php" ?>
      <div class="container-center">
           <h1 class="main-heading">Interview: Rowan Laffeber</h1>
           <h2 class="Interview-heading"> Why Software development and not another training?</h2>
