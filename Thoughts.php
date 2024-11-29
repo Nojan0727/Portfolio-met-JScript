4de19310e9aa5954ba80a7d3f46db5cf058f6c12
Nojan0727 <9025261@student.zadkine.nl>
2024-11-29 19:05:22 +0100

    switch thought html to php

--- Thoughts.php
+++ Thoughts.php
@@ -1,24 +1,4 @@
-<!doctype html>
-<html lang="en">
-    <head>
-       <meta charset="UTF-8">
-       <meta name="viewport" content="Thoughts">
-       <title>Thoughts</title>
-       <link rel="stylesheet" href="main.css">
-        <script defer src="script.js"></script>
-    </head>
-<body class="body-background">
-     <nav class="navbar">
-        <a class="nav-link" href="Index.php"><img src="image/Logo.png" alt="Logo" class="nav-logo"></a>
-        <a class="nav-link" href="About_me.php">About me</a>
-        <a class="nav-link" href="Professional_Experience.php">Professional experiences</a>
-        <a class="nav-link" href="Skills.php">Skills</a>
-        <a class="nav-link" href="Hobby.php">Hobby</a>
-        <a class="nav-link" href="Certifications.php">Certifications</a>
-        <a class="nav-link" href="Interview.php">Interview</a>
-        <a class="nav-link active" href="Thoughts.php">Thoughts</a>
-        <a class="nav-link" href="Contact.php">Contact</a>
-     </nav>
+<?php require "partials/header.php" ?>
       <div class="container-center">
            <h1 class="main-heading">The Future of IT</h1>
            <p class="text-paragraph">An article discussing emerging trends in the tech industry, my thoughts on future developments, and how they may impact my career trajectory.</p>
