2e4efef13a646771730d01512e09f2d40c2f4c79
Nojan0727 <9025261@student.zadkine.nl>
2024-11-29 19:06:14 +0100

    created partials folder for php

---
+++ partials/header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nojan's Portfolio</title>
+    <link rel="stylesheet" href="main.css">
+    <script defer src="script.js"></script>
+</head>
+<body class="body-background">
+
+<nav class="navbar">
+    <a class="nav-link" href="Index.php"><img src="image/Logo.png" alt="Logo" class="nav-logo"></a>
+    <a class="nav-link" href="About_me.php">About me</a>
+    <a class="nav-link" href="Professional_Experience.php">Professional experiences</a>
+    <a class="nav-link" href="Skills.php">Skills</a>
+    <a class="nav-link" href="Hobby.php">Hobby</a>
+    <a class="nav-link" href="Certifications.php">Certifications</a>
+    <a class="nav-link" href="Interview.php">Interview</a>
+    <a class="nav-link" href="Thoughts.php">Thoughts</a>
+    <a class="nav-link" href="Contact.php">Contact</a>
+    <label class="lan-select" for="language-selector"><select id="language-selector" class="nav-dropdown">
+            <option value="en">English</option>
+            <option value="nl">Dutch</option>
+        </select>
+    </label>
+    <button id="dark-mode-toggle" class="toggleDarkMode" onclick="toggleDarkMode()">🌙</button>
+</nav>
