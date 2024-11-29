7ec235a010e6b45e7f4cee0d4849f5b6c5a32016
Nojan0727 <9025261@student.zadkine.nl>
2024-11-29 19:05:39 +0100

    switch index html to php

--- Index.php
+++ Index.php
@@ -1,31 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nojan's Portfolio</title>
-    <link rel="stylesheet" href="main.css">
-    <script defer src="script.js"></script>
-</head>
-<body class="body-background">
-
-<nav class="navbar">
-    <a class="nav-link" href="Index.php"><img src="image/Logo.png" alt="Logo" class="nav-logo"></a>
-    <a class="nav-link" href="About_me.php">About me</a>
-    <a class="nav-link" href="Professional_Experience.php">Professional experiences</a>
-    <a class="nav-link" href="Skills.php">Skills</a>
-    <a class="nav-link" href="Hobby.php">Hobby</a>
-    <a class="nav-link" href="Certifications.php">Certifications</a>
-    <a class="nav-link" href="Interview.php">Interview</a>
-    <a class="nav-link" href="Thoughts.php">Thoughts</a>
-    <a class="nav-link" href="Contact.php">Contact</a>
-    <label class="lan-select" for="language-selector"><select id="language-selector" class="nav-dropdown">
-        <option value="en">English</option>
-        <option value="nl">Dutch</option>
-    </select>
-    </label>
-    <button id="dark-mode-toggle" class="toggleDarkMode" onclick="toggleDarkMode()">🌙</button>
-</nav>
+<?php require "partials/header.php" ?>
 <div class="cookies-wall" id="cookie-wall">
     <h2> cookie melding</h2>
     <p>LET OP"</p>
@@ -34,7 +7,6 @@
     <button onclick="acceptCookies()" class="act-button"> Accepteer</button>
     <button onclick="closeCookieBanner()" class="act-button">Verwijder</button>
 </div>
-
 <div class="container-center">
     <h1 class="main-heading">Hi, Welcome to my home page</h1>
     <h2 class="second-heading">Nojan Alinaghi - Software Developer</h2>
