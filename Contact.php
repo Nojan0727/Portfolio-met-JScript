44563d7c3c8ca8008d28174effddeadda8cc203c
Nojan0727 <9025261@student.zadkine.nl>
2024-11-29 19:03:34 +0100

    switch contact html to php

--- Contact.php
+++ Contact.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="Contact">
-  <title>Contact Us</title>
-  <link rel="stylesheet" href="main.css">
-    <script defer src="script.js"></script>
-</head>
-<body class="body-background">
-   <nav class="navbar">
-      <a class="nav-link" href="Index.php"><img src="image/Logo.png" alt="Logo" class="nav-logo"></a>
-      <a class="nav-link" href="About_me.php">About me</a>
-      <a class="nav-link" href="Professional_Experience.php">Professional experiences</a>
-      <a class="nav-link" href="Skills.php">Skills</a>
-      <a class="nav-link" href="Hobby.php">Hobby</a>
-      <a class="nav-link" href="Certifications.php">Certifications</a>
-      <a class="nav-link" href="Interview.php">Interview</a>
-      <a class="nav-link" href="Thoughts.php">Thoughts</a>
-      <a class="nav-link active" href="Contact.php">Contact</a>
-   </nav>
+<?php require "partials/header.php" ?>
 <header class="container-center">
       <h1 class="contact-heading">Contact Me</h1>
   <form class="contact-form">
