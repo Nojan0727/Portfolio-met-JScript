d13585404c3596db510b348fb5202f1256df3b4e
Nojan0727 <9025261@student.zadkine.nl>
2024-11-29 19:02:15 +0100

    switch About me html to php

--- About_me.php
+++ About_me.php
@@ -1,24 +1,4 @@
-<!doctype html>
-<html lang="en">
-   <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="About me page of my portfolio">
-        <title>About me</title>
-        <link rel="stylesheet" href="main.css">
-       <script defer src="script.js"></script>
-   </head>
-<body class="body-background">
-<nav class="navbar">
-    <a class="nav-link" href="Index.php"><img src="image/Logo.png" alt="Logo" class="nav-logo"></a>
-    <a class="nav-link" href="About_me.php">About me</a>
-    <a class="nav-link" href="Professional_Experience.php">Professional experiences</a>
-    <a class="nav-link" href="Skills.php">Skills</a>
-    <a class="nav-link" href="Hobby.php">Hobby</a>
-    <a class="nav-link" href="Certifications.php">Certifications</a>
-    <a class="nav-link" href="Interview.php">Interview</a>
-    <a class="nav-link" href="Thoughts.php">Thoughts</a>
-    <a class="nav-link" href="Contact.php">Contact</a>
-</nav>
+<?php require "partials/header.php" ?>
    <div class="container-center ">
        <img src="image/Selfie.jpeg" alt="selfie" class="selfie">
        <h1 class="main-heading">About me</h1>
